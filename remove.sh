211f048effc4a3961e2fe08f91aed45f00f4f0f9
Fuheng Wu <fuheng.wu@oracle.com>
2021-07-02 19:24:38 -0700

    add bash script

---
+++ remove.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -x
+
+kubectl delete namespace knative-eventing &
+kubectl delete namespace cert-manager &
+kubectl delete namespace istio-system &
+kubectl delete namespace auth &
+kubectl delete namespace knative-serving &
+kubectl delete namespace kubeflow &
+kubectl delete namespace kubeflow-user-example-com &
+
