fbce23ff1a1e59c3b6fc08a14667c8bcc77fd33b
Fuheng Wu <fuheng.wu@oracle.com>
2021-07-03 15:09:13 -0700

    rename namespace

--- remove.sh
+++ remove.sh
@@ -2,11 +2,21 @@
 
 set -x
 
-kubectl delete namespace knative-eventing &
-kubectl delete namespace cert-manager &
-kubectl delete namespace istio-system &
-kubectl delete namespace auth &
-kubectl delete namespace knative-serving &
-kubectl delete namespace kubeflow &
-kubectl delete namespace oracle-ocas-vision &
+#kubectl delete namespace knative-eventing &
+#kubectl delete namespace cert-manager &
+#kubectl delete namespace istio-system &
+#kubectl delete namespace auth &
+#kubectl delete namespace knative-serving &
+#kubectl delete namespace kubeflow &
+#kubectl delete namespace kubeflow-user-example-com &
+#kubectl delete namespace oracle-ocas-gamma &
 
+
+for ns in auth knative-eventing cert-manager kubeflow knative-serving kubeflow-user-example-com istio-system oracle-ocas-gamma oracle-ocas-vision; do
+
+kubectl delete namespace $ns &
+curl -k -H "Content-Type: application/json" -X PUT --data "{ \"kind\": \"Namespace\", \"apiVersion\": \"v1\", \"metadata\": { \"name\": \"$ns\" }, \"spec\": { \"finalizers\": [] } }" \
+ http://127.0.0.1:8001/api/v1/namespaces/$ns/finalize
+
+
+done
